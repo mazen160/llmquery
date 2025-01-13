ee9c753d6c434cc6ed756283c31fda4c07cd13dd
Mazin Ahmed <mazin@mazinahmed.net>
2025-01-13 01:03:56 -0800

    wip

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="llmquery",
-    version="0.1.3",
+    version="0.1.6",
     author="Mazin Ahmed",
     author_email="mazin@mazinahmed.net",
     description="A package for querying various LLM providers",
@@ -33,9 +33,7 @@ setup(
                                     'providers.google_gemini_lib*',
                                     'providers.openai_lib*',
                                     'providers.ollama_lib*']),
-    package_data={
-        'llmquery': ['llmquery-templates/*', 'llmquery-templates/**/*'],
-    },
+    data_files=[("llmquery-templates", templates_files)],
     entry_points={'console_scripts': ['llmquery=llmquery.__main__:main']},
     include_package_data=True,
     install_requires=required,
