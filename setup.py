6d027b9ae01f68d189c80dd9d9cad36a35d6e69d
Mazin Ahmed <mazin@mazinahmed.net>
2025-01-13 00:40:42 -0800

    wip

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="llmquery",
-    version="0.1.2",
+    version="0.1.3",
     author="Mazin Ahmed",
     author_email="mazin@mazinahmed.net",
     description="A package for querying various LLM providers",
